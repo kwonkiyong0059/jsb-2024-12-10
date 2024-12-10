ac114170a2eb81be8d91777a781e3fafe89d80bc
kwonkiyong0059 <kkoung425@gmail.com>
2024-12-10 12:18:13 +0900

    2-05-6

--- src/main/java/com/kkd/sbb/repository/QuestionRepository.java
+++ src/main/java/com/kkd/sbb/repository/QuestionRepository.java
@@ -3,6 +3,9 @@ package com.kkd.sbb.repository;
 import com.kkd.sbb.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
+    Question findBySubjectAndContent(String subject, String content);
 }
--- src/test/java/com/kkd/sbb/SbbApplicationTests.java
+++ src/test/java/com/kkd/sbb/SbbApplicationTests.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -15,9 +18,24 @@ class SbbApplicationTests {
     private QuestionRepository questionRepository;
 
     @Test
-    void testJpa(){
+    void testJpa1(){
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
+    }
+
+    @Test
+    void testJpa2(){
         Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
         assertEquals(1, q.getId());
     }
 
+    @Test
+    void testJpa3() {
+        Question q = this.questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1, q.getId());
+    }
 }
