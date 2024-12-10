5e590ff88811ae11166bcf2ba8435149232bfa9f
kwonkiyong0059 <kkoung425@gmail.com>
2024-12-10 15:19:46 +0900

    2-07-3

--- src/main/java/com/kkd/sbb/controller/QuestionController.java
+++ src/main/java/com/kkd/sbb/controller/QuestionController.java
@@ -1,13 +1,24 @@
 package com.kkd.sbb.controller;
 
+import com.kkd.sbb.entity.Question;
+import org.springframework.ui.Model;
+import com.kkd.sbb.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
 
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    public String list() {
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
         return "question_list";
     }
 }
