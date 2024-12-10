c692652cfa74b1b7d4669107f2e835ad499d7210
kwonkiyong0059 <kkoung425@gmail.com>
2024-12-10 11:44:17 +0900

    2-05-4

--- src/main/java/com/kkd/sbb/repository/QuestionRepository.java
+++ src/main/java/com/kkd/sbb/repository/QuestionRepository.java
@@ -4,5 +4,5 @@ import com.kkd.sbb.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-
+    Question findBySubject(String subject);
 }
--- src/test/java/com/kkd/sbb/SbbApplicationTests.java
+++ src/test/java/com/kkd/sbb/SbbApplicationTests.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -18,11 +16,8 @@ class SbbApplicationTests {
 
     @Test
     void testJpa(){
-        Optional<Question> oq = this.questionRepository.findById(1);
-        if(oq.isPresent()) {
-            Question q = oq.get();
-            assertEquals("sbb가 무엇인가요?", q.getSubject());
-        }
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
     }
 
 }
