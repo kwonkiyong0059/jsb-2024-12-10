50f3811eaef217bf6cd6f7fe84293a622df5ee1c
kwonkiyong0059 <kkoung425@gmail.com>
2024-12-10 17:14:43 +0900

    2-10

--- src/main/java/com/kkd/sbb/question/QuestionService.java
+++ src/main/java/com/kkd/sbb/question/QuestionService.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import com.kkd.sbb.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +16,16 @@ public class QuestionService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+
+        try {
+            return question.orElseThrow(() -> new DataNotFoundException("question not found"));
+        } catch (DataNotFoundException e) {
+            throw e;
+        }
+
     }
 }
