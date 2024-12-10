df4a98f373fd000c0dae7a09a2347055adab4208
kwonkiyong0059 <kkoung425@gmail.com>
2024-12-10 11:37:42 +0900

    2-05-1

---
+++ src/main/java/com/kkd/sbb/repository/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.kkd.sbb.repository;
+
+import com.kkd.sbb.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ src/main/java/com/kkd/sbb/repository/QuestionRepository.java
@@ -0,0 +1,8 @@
+package com.kkd.sbb.repository;
+
+import com.kkd.sbb.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+}
