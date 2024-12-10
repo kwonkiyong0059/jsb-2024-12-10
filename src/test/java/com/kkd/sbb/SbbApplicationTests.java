66445351c057ee8ba67f63831cf5cdf791807a1c
kwonkiyong0059 <kkoung425@gmail.com>
2024-12-10 12:30:44 +0900

    2-05-7

--- src/test/java/com/kkd/sbb/SbbApplicationTests.java
+++ src/test/java/com/kkd/sbb/SbbApplicationTests.java
@@ -38,4 +38,14 @@ class SbbApplicationTests {
                 "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
         assertEquals(1, q.getId());
     }
+
+    //sbb% : 'sbb'로 시작하는 문자열
+    //%sbb : 'sbb'로 끝나는 문자열
+    //%sbb% : 'sbb'를 포함하는 문자열
+    @Test
+    void testJpa4() {
+        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
 }
